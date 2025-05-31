c7f4757f9ec782463216e5e661e581e5b958f3b5
Your Name <you@example.com>
2025-05-30 22:04:22 -0300

    icon y splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'la_comanda',
+  appId: 'com.angularianos.comanda',
+  appName: 'la comanda',
   webDir: 'www'
 };
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, CommonModule],
 })
 export class AppComponent {
-  constructor() {}
+  showSplash = true;
+
+  constructor() { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.showSplash = false;
+    }, 3000);
+  }
 }
